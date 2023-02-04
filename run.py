e0a448725ee3608ef6fc854717e9c3af161ffc07
Zonylaw <sunny_law@hotmail.com>
2023-02-04 18:54:58 +0000

    add integer validation

--- run.py
+++ run.py
@@ -12,8 +12,37 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('my_sandwiches')
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
-print(data)
+# sales = SHEET.worksheet('sales')
+# data = sales.get_all_values()
+# print(data)
 
+def get_sales_data():
+    """"
+    Get sales figures input form the user
+    """
+    print("Please enter sales data from the last market")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60")
+    data_str = input("Enter your data here:")
+    print(f"The data provided is {data_str}")
+    sales_data = data_str.split(",")
+    print(sales_data)
+    validate_date(sales_data)
+
+def validate_date(values):
+    """
+    check that there are 6 values
+    check that the values can be converted to integers
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+get_sales_data()
 
